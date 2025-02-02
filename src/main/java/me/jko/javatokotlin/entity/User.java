b819a5e3ef578e22036f7087c7408af3f06db7b4
junhee-ko <junheee.ko@gmail.com>
2025-02-02 18:25:49 +0900

    Add User entity

---
+++ src/main/java/me/jko/javatokotlin/entity/User.java
@@ -0,0 +1,24 @@
+package me.jko.javatokotlin.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity(name = "user_test")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+}
